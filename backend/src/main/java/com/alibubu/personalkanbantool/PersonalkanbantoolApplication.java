3bc5c3af3fdcc918eb6facdac6e45df2a7f4014e
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-20 17:01:23 +0800

    backend init

---
+++ backend/src/main/java/com/alibubu/personalkanbantool/PersonalkanbantoolApplication.java
@@ -0,0 +1,13 @@
+package com.alibubu.personalkanbantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalkanbantoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalkanbantoolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alibubu/personalkanbantool/PersonalkanbantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibubu.personalkanbantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalkanbantoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
