55f95e706793c70d0d4109e2f7204405b71d35ea
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-21 01:26:59 +0800

    1st entity & repository

---
+++ backend/src/main/java/com/alibubu/personalkanbantool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.alibubu.personalkanbantool.repositories;
+
+import com.alibubu.personalkanbantool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+//Crud offers easy CRUD operation
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    public Iterable<Project> findAllById(Iterable<Long> iterable);
+}
