f4a72fe6132739f00440887f3a9db31538106d3a
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-23 00:17:25 +0800

    first service

---
+++ backend/src/main/java/com/alibubu/personalkanbantool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package com.alibubu.personalkanbantool.services;
+
+import com.alibubu.personalkanbantool.domain.Project;
+import com.alibubu.personalkanbantool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        //Logic
+        return projectRepository.save(project);
+    }
+}
