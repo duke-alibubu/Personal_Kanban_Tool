622773e995c56bf4c4f00b1a17ded11480b911bf
Trinh Dung <c170017@e.ntu.edu.sg>
2020-03-25 23:29:44 +0800

    use BindingResult to detect errors

--- backend/src/main/java/com/alibubu/personalkanbantool/web/ProjectController.java
+++ backend/src/main/java/com/alibubu/personalkanbantool/web/ProjectController.java
@@ -5,10 +5,13 @@ import com.alibubu.personalkanbantool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,10 +19,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+
+    //BindingResult: basically it analyzes the object & it determines whether or not there are errors
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>("Invalid Project Object!", HttpStatus.BAD_REQUEST);
+        }
+
         Project createdProject = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 
 
