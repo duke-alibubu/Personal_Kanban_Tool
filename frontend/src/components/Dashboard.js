00ea447875e544bafc40acce7def977ec827ba43
Trinh Dung <c170017@e.ntu.edu.sg>
2020-04-30 18:38:42 +0800

    template

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ProjectItem from './project/ProjectItem'
+import Header from './layout/Header'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <h1>
                     Welcome to the Dashboard!
                 </h1>
