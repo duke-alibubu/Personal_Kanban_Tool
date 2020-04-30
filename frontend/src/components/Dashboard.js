49f286570c1ebce27ddd33ed822fa204463c8602
Trinh Dung <c170017@e.ntu.edu.sg>
2020-04-30 18:36:30 +0800

    proj item

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <h1>
-                Con cac
-            </h1>
+            <div>
+                <h1>
+                    Welcome to the Dashboard!
+                </h1>
+                <ProjectItem />
+            </div>
         )
     }
 }
