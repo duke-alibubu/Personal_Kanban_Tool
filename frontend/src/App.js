965080355e979f5d5d0030d92c315df3feecdc99
Trinh Dung <c170017@e.ntu.edu.sg>
2020-05-01 21:14:37 +0800

    redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import AddProject from './components/project/AddProject';
+import { Provider } from "react-redux";
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
-
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
