3350e4490300441b4ce0bd795f1c3423a28f0b91
Trinh Dung <c170017@e.ntu.edu.sg>
2020-05-08 16:14:28 +0800

    Prohect Task Service add proj task

--- backend/src/main/java/com/alibubu/personalkanbantool/services/ProjectTaskService.java
+++ backend/src/main/java/com/alibubu/personalkanbantool/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.alibubu.personalkanbantool.services;
 
+import com.alibubu.personalkanbantool.domain.Backlog;
 import com.alibubu.personalkanbantool.domain.ProjectTask;
 import com.alibubu.personalkanbantool.repositories.BacklogRepository;
 import com.alibubu.personalkanbantool.repositories.ProjectTaskRepository;
@@ -14,17 +15,30 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
         //PTs to be added to a specific non-null project, Backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
         //set backlog to the project task
-
-
+        projectTask.setBacklog(backlog);
 
         //IDPROJECT-1 (ID within the project only). We want our project sequence to be like this
+        Integer backlogSeq = backlog.getPtSequence();
+
         //update the backlog sequence
+        backlogSeq++;
+
+        //add PtSeq to the task
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSeq);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         //initial priority when priority is NULL
-        //initial status when status is NULL
+        if (projectTask.getPriority() == null || projectTask.getPriority() == 0)
+            projectTask.setPriority(3);
 
+        //initial status when status is NULL
+        if (projectTask.getStatus() == null || projectTask.getStatus() == "")
+            projectTask.setStatus("TO_DO");
+
+        return projectTaskRepository.save(projectTask);
     }
 }
